e6d676d88e1dc87b2c106d9effd6ad8ce93bcf49
Asem Edress <asem.edress@gmail.com>
2022-11-12 04:45:08 +0100

    fixing the job route

--- routes/job.routes.js
+++ routes/job.routes.js
@@ -5,6 +5,20 @@ const Job = require("../models/Jobs.model");
 
 const { isLoggedIn } = require("../middleware/jwt.middleware");
 
+jobRouter.get("/", (req, res) => {
+  res.json("all good here as well");
+});
+
+jobRouter.get("/all", (req, res) => {
+  const id = req.params._id;
+  Job.find(id).then((job) => {
+    if (!job) {
+      return res.status(400).json({ errorMessage: "No jobs were found" });
+    }
+    res.json({ job });
+  });
+});
+
 jobRouter.post("/create", isLoggedIn, (req, res, next) => {
   const { title, location, description, seniority, tech } = req.body;
 
@@ -25,21 +39,6 @@ jobRouter.post("/create", isLoggedIn, (req, res, next) => {
     .catch((err) => {
       next(err);
     });
-});
-
-jobRouter.get("/all", async (req, res, next) => {
-  const id = req.params._id;
-  try {
-    const jobPosts = await Job.find(id);
-
-    if (jobPosts) {
-      res.json({ jobPosts });
-    } else {
-      next();
-    }
-  } catch (err) {
-    next(err);
-  }
 });
 
 jobRouter.get("/creator/:id", async (req, res, next) => {
